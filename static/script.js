18454b5dc2654fe7c63dd6260913617429cc8d62
henmikazuki <minskeh@gmail.com>
2026-04-25 22:53:30 +0900

    回答を入力できるように機能を追加

--- static/script.js
+++ static/script.js
@@ -1,6 +1,23 @@
+let currentAnswer = "";
+
 async function loadQuestion() {
   const res = await fetch("/api/start");
   const data = await res.json();
 
-  document.getElementById("question").innerText = data.questions[0].question;
+  const q = data.questions[0];
+
+  currentAnswer = q.answer;
+
+  document.getElementById("questionButton").innerText = "次の問題";
+  document.getElementById("question").innerText = q.question;
+}
+
+function submitAnswer() {
+  const input = document.getElementById("answer").value;
+
+  if (input.toUpperCase() === currentAnswer) {
+    document.getElementById("result").innerText = "正解！";
+  } else {
+    document.getElementById("result").innerText = "不正解: " + currentAnswer;
+  }
 }
