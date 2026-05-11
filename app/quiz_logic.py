477e3dfaeae4b451ae2f2481e9e28207122c4e97
henmikazuki <minskeh@gmail.com>
2026-05-11 22:42:31 +0900

    docstringを追加

--- app/quiz_logic.py
+++ app/quiz_logic.py
@@ -3,6 +3,12 @@ from .morse_data import MORSE
 
 
 def generate_quiz_set(count=10):
+    """クイズの問題セットを生成する関数
+    Args:
+        count (int, optional): 生成する問題の数. Defaults to 10.
+    Returns:
+        list: クイズの問題セット
+    """
     questions = []
 
     letters = list(MORSE.keys())
